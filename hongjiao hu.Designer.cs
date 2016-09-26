acd2519bcae1c52486edc5ceb4bbf7d31114d291
hongjiaohu <h.hu@sjasd.ca>
2016-09-26 10:34:57 -0500

    compete first question
    finish product

---
+++ hongjiao hu.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace hongjiao_hu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Hellouserprogram = new System.Windows.Forms.Label();
+            this.bigbox = new System.Windows.Forms.GroupBox();
+            this.b1 = new System.Windows.Forms.Button();
+            this.asd = new System.Windows.Forms.Label();
+            this.bigbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Hellouserprogram
+            // 
+            this.Hellouserprogram.AutoSize = true;
+            this.Hellouserprogram.Location = new System.Drawing.Point(311, 9);
+            this.Hellouserprogram.Name = "Hellouserprogram";
+            this.Hellouserprogram.Size = new System.Drawing.Size(103, 13);
+            this.Hellouserprogram.TabIndex = 0;
+            this.Hellouserprogram.Text = "what are you doing?";
+            // 
+            // bigbox
+            // 
+            this.bigbox.Controls.Add(this.b1);
+            this.bigbox.Location = new System.Drawing.Point(101, 77);
+            this.bigbox.Name = "bigbox";
+            this.bigbox.Size = new System.Drawing.Size(507, 372);
+            this.bigbox.TabIndex = 1;
+            this.bigbox.TabStop = false;
+            this.bigbox.Enter += new System.EventHandler(this.bigbox_Enter);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(238, 158);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(75, 23);
+            this.b1.TabIndex = 0;
+            this.b1.Text = "b1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // asd
+            // 
+            this.asd.AutoSize = true;
+            this.asd.Location = new System.Drawing.Point(356, 61);
+            this.asd.Name = "asd";
+            this.asd.Size = new System.Drawing.Size(0, 13);
+            this.asd.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 461);
+            this.Controls.Add(this.asd);
+            this.Controls.Add(this.bigbox);
+            this.Controls.Add(this.Hellouserprogram);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.bigbox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Hellouserprogram;
+        private System.Windows.Forms.GroupBox bigbox;
+        private System.Windows.Forms.Label asd;
+        private System.Windows.Forms.Button b1;
+    }
+}
+
---
+++ hongjiao hu.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hongjiao_hu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            asd.Text = "good";
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            asd.Text = "bad";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            asd.Text = "ture";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            asd.Text = "flase";
+        }
+
+        private void bigbox_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            asd.Text = "correct";
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            asd.Text = "incorrect";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            asd.Text = "good";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            asd.Text = "bad";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            asd.Text = "ture";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            asd.Text = "flase";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            asd.Text = "correct";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            asd.Text = "incorrect";
+        }
+    }
+}
